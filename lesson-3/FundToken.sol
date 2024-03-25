373935daec41ddef65859bf6546c0a19f98c4369
Frank Kong <frankkongnj@gmail.com>
2024-03-25 20:38:12 +0800

    update codes for lesson2

--- lesson-3/FundToken.sol
+++ lesson-3/FundToken.sol
@@ -1,41 +1,40 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.19;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 contract FundToken {
-    // name: FundToken
-    // symbol: FT
-    // totalSupply: 1000
-    // owner
-    // balances: holder -> amount
-    // functions: transfer, getBalance, mint, burn
-
-    string public TOKEN_NAME;
-    string public SYMBOL;
-    uint256 public totalSupply = 1000 * 10 ** 18;
-    mapping(address holder => uint256 balance) public balance;
+    // 1. 通证的名字
+    // 2. 通证的简称
+    // 3. 通证的发行数量
+    // 4. owner地址
+    // 5. balance address => uint256
+    string public tokenName;
+    string public tokenSymbol;
+    uint256 public totalSupply;
     address public owner;
+    mapping(address => uint256) public balances;
 
-    constructor(string memory tokenName, string memory symbol) {
-        TOKEN_NAME = tokenName;
-        SYMBOL = symbol;
+    constructor(string memory _tokenName, string memory _tokenSymbol) {
+        tokenName = _tokenName;
+        tokenSymbol = _tokenSymbol;
         owner = msg.sender;
-        balance[owner] = totalSupply;
     }
 
-    function balanceOf(address addrToCheck) public view returns (uint256) {
-        return balance[addrToCheck];
-    }
-
-    function transfer(address payee, uint256 amountToTransfer) public {
-        require(balanceOf(msg.sender) >= amountToTransfer, "You do not have sufficient balance");
-        balance[msg.sender] -= amountToTransfer;
-        balance[payee] += amountToTransfer;
-    }
-
+    // mint: 获取通证
     function mint(uint256 amountToMint) public {
-        require(msg.sender == owner, "You are not allowed to mint tokens");
-        balance[msg.sender] += amountToMint;
+        balances[msg.sender] += amountToMint;
         totalSupply += amountToMint;
     }
+
+    // transfer: transfer 通证
+    function transfer(address payee, uint256 amount) public {
+        require(balances[msg.sender] >= amount, "You do not have enough balance to transfer");
+        balances[msg.sender] -= amount;
+        balances[payee] += amount;
+    } 
+
+    // balanceOf: 查看某一个地址的通证数量
+    function balanceOf(address addr) public view returns(uint256) {
+        return balances[addr];
+    }
+
 }
--- lesson-3/FundTokenERC20.sol
+++ lesson-3/FundTokenERC20.sol
@@ -1,25 +1,34 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// FundMe
+// 1. 让FundMe的参与者，基于 mapping 来领取相应数量的通证
+// 2. 让FundMe的参与者，transfer 通证
+// 3. 在使用完成以后，需要burn 通证
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {FundMe} from "./FundMe.sol";
 
-contract MyToken is ERC20 {
-    address owner;
-    FundMe fm;
-    constructor(address fundMeAddr) ERC20("MyToken", "MTK") {
-        owner = msg.sender;
-        fm = FundMe(fundMeAddr);
+contract FundTokenERC20 is ERC20 {
+    FundMe fundMe;
+    constructor(address fundMeAddr) ERC20("FundTokenERC20", "FT") {
+        fundMe = FundMe(fundMeAddr);
     }
 
     function mint(uint256 amountToMint) public {
-        require(fm.listOfFunders(msg.sender) >= amountToMint, "You don't have enough points to claim");
-        require(fm.fundSuccess(), "fundraising failed");
+        require(fundMe.fundersToAmount(msg.sender) >= amountToMint, "You cannot mint this many tokens");
+        require(fundMe.getFundSuccess(), "The fundme is not completed yet");
         _mint(msg.sender, amountToMint);
+        fundMe.setFunderToAmount(msg.sender, fundMe.fundersToAmount(msg.sender) - amountToMint);
     }
 
-    function burn(uint256 amountToBurn) public {
-        require(balanceOf(msg.sender) >= amountToBurn, "You do not have enough balance.");
-        _burn(msg.sender, amountToBurn);
+    function claim(uint256 amountToClaim) public {
+        // complete cliam
+        require(balanceOf(msg.sender) >= amountToClaim, "You dont have enough ERC20 tokens");
+        require(fundMe.getFundSuccess(), "The fundme is not completed yet");
+        /*to add */
+        // burn amountToClaim Tokens       
+        _burn(msg.sender, amountToClaim);
     }
 }
